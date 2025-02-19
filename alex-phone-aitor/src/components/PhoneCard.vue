04ed4c960c6e14e554692275c945b7385145330f
Aitor-100 <aitorarcedarodriguez1999@gmail.com>
2025-02-19 16:52:11 +0100

    Peticion a la API y componente Phone mostado en el Home

---
+++ alex-phone-aitor/src/components/PhoneCard.vue
@@ -0,0 +1,68 @@
+<script setup>
+defineProps(["product"]);
+</script>
+
+<template>
+  <div class="card">
+    <img :src="product.image" :alt="product.name" />
+    <div class="phone-detail">
+      <h2>{{ product.name }}</h2>
+      <p class="detail"><strong>Grade:</strong> {{ product.grade }}</p>
+      <p class="detail"><strong>Color:</strong> {{ product.color }}</p>
+      <p class="detail"><strong>Storage:</strong> {{ product.storage }}</p>
+      <p class="price"><strong>${{ product.price }}</strong></p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border: 1px solid #ddd;
+  padding: 16px;
+  border-radius: 8px;
+  text-align: center;
+  width: 250px;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+  background-color: white;
+
+  cursor: pointer;
+  transition: transform 0.3s ease-in-out;
+}
+.card img {
+  width: 100%;
+  height: 50%; /* Ajusta la altura según necesites */
+  object-fit: contain;
+  border-radius: 8px;
+}
+
+
+.phone-detail{
+ text-align: left;
+ padding: 0 1rem;
+ height: 50%;
+
+ display: flex;
+ flex-direction: column; 
+ justify-content: flex-start;
+}
+
+.price{
+  margin-top: auto; 
+}
+
+
+.detail {
+  font-size: 0.9rem;
+  color: #555;
+  margin: 4px 0;
+}
+
+.detail strong, .price strong{
+  color: #333;
+  font-weight: bold;
+}
+
+.card:hover{
+  transform: scale(1.1);
+}
+</style>
--- alex-phone-aitor/src/views/HomeView.vue
+++ alex-phone-aitor/src/views/HomeView.vue
@@ -1,8 +1,38 @@
 <script setup>
+import { ref, onMounted } from "vue";
+import PhoneCard from "@/components/PhoneCard.vue";
+
+const products = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://test.alexphone.com/api/v1/skus");
+    const data = await response.json();
+    products.value = data;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+});
 </script>
 
 <template>
   <main>
-    <h1>Home</h1>
+    <div class="search">
+
+    </div>
+    <div class="product-list">
+      <PhoneCard v-for="product in products" :key="product.id" :product="product" />
+    </div>
   </main>
 </template>
+
+<style scoped>
+.product-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  justify-content: center;
+
+  margin: 2rem;
+}
+</style>
